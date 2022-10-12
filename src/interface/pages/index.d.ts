fc2e035a012141dfeabe130484942a15e9709746
WengX-unx <1027382710@qq.com>
2022-10-12 18:31:32 +0800

    refactor: remove the contents of the home menu

--- src/interface/pages/index.d.ts
+++ src/interface/pages/index.d.ts
@@ -2,12 +2,8 @@ export interface QueryProps {
    allMarkdownRemark: {
       edges: {
          node: {
-            excerpt: string;
-            timeToRead: number;
             frontmatter: {
-               date: string;
                title: string;
-               tag: string[];
             };
          };
       }[];
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,49 +1,19 @@
 import * as React from 'react';
 import type { HeadFC } from 'gatsby';
-import { graphql } from 'gatsby';
-import { QueryProps } from '../interface/pages';
 
 import Layout from '../layout';
 
 import './styles/index.scss';
 
-const IndexPage = ({ data }: { data: QueryProps }) => {
-   console.log(data);
-
+const IndexPage = () => {
    return (
       <Layout>
          <aside className="index-menu"></aside>
-         <article className="index-article">
-            {data.allMarkdownRemark.edges.map((item, index) => (
-               <div key={index} className="post">
-                  <h2 className="title text-primary">{item.node.frontmatter.title}</h2>
-                  <p className="excerpt">{item.node.excerpt}</p>
-               </div>
-            ))}
-         </article>
+         <article className="index-article"></article>
       </Layout>
    );
 };
 
 export default IndexPage;
-
-export const query = graphql`
-   {
-      allMarkdownRemark {
-         edges {
-            node {
-               frontmatter {
-                  title
-                  date
-                  tag
-               }
-               excerpt(pruneLength: 100)
-               timeToRead
-            }
-         }
-         totalCount
-      }
-   }
-`;
 
 export const Head: HeadFC = () => <title>Blog - WengX</title>;
