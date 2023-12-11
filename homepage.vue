9a612342dd3516684346c746d2a1fec33f55ff15
WengX <1027382710@qq.com>
2023-12-11 18:03:48 +0800

    init

---
+++ homepage.vue
@@ -0,0 +1,22 @@
+<script setup>
+const theme = () => {
+  document.querySelector("html").classList.toggle("dark");
+};
+</script>
+
+<template>
+  <div class="wengx-home-page">
+    <!-- <button class="theme-btn" @click="theme">dark/light</button> -->
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.wengx-home-page {
+  width: 100%;
+  height: 100vh;
+  background-color: rgb(187, 187, 187);
+}
+.theme-btn {
+  background-color: red;
+}
+</style>
---
+++ index.md
@@ -0,0 +1,13 @@
+---
+layout: page
+title: "Home"
+pageClass: home-page
+navbar: false
+sidebar: false
+---
+
+<script setup>
+import HomePage from './homepage.vue'
+</script>
+
+<HomePage />
